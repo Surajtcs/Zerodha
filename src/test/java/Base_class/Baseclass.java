93ff4c21251487eac5a68d66ca286121fffc8701
Admin <Admin@DESKTOP-UA4SH76>
2022-08-31 21:28:24 +0530

    My first code

---
+++ src/test/java/Base_class/Baseclass.java
@@ -0,0 +1,21 @@
+package Base_class;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Baseclass {
+	public WebDriver driver;
+	public void initilizeBrowser() {
+		WebDriverManager.chromedriver().setup();
+	    driver=new ChromeDriver();
+	    driver.get("https://kite.zerodha.com/");
+	    //maximize
+	    driver.manage().window().maximize();
+	    //implicitly wait
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	}
+}
---
+++ src/test/java/Utility/Utility_class.java
@@ -0,0 +1,15 @@
+package Utility;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class Utility_class {
+	public static String getTD(int rowindex,int colindex) throws Throwable {
+		FileInputStream fis = new FileInputStream("E:\\Software testing\\automation testing\\selenium\\zerodha.xlsx");
+		Sheet sh = WorkbookFactory.create(fis).getSheet("Sheet1");
+		String data = sh.getRow(rowindex).getCell(colindex).getStringCellValue();
+		return data;
+}
+}
